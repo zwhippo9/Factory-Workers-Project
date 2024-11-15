80f92ed32c3951a1ca7cffc5faea1061bf16aa67
zwhippo9 <zwhippo9@gmail.com>
2024-11-15 00:48:26 +0000

    5

--- .ccls-cache/@home@runner@Factory-Workers-Project/main.cpp
+++ .ccls-cache/@home@runner@Factory-Workers-Project/main.cpp
@@ -1,64 +1,71 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
+//employee class derived
 class Employee {
-private:
-    std::string name;  //name of employee
-    int idNumber;      //employee ID number
-    std::string hireDate;  //date of hire
+protected:
+    string name;
+    int employeeID;
+    string department;
 
 public:
+    //constructor
+Employee(string n = "", int id = 0, string dep = "") 
+: name(n), employeeID(id), department(dep) {}
 
-    Employee() : name(""), idNumber(0), hireDate("") {}
+    //accesors
+    string getName() const { return name; }
+    int getEmployeeID() const { return employeeID; }
+    string getDepartment() const { return department; } 
 
+    //mutators
+    void setName(const string& n) { name = n; }
+    void setEmployeeID(int id) { employeeID = id; }
+    void setDepartment(const string& dep) { department = dep; }
 
-    Employee(const std::string& empName, int empidNumber, const std::string& empHireDate)
-        : name(empName), idNumber(empidNumber), hireDate(empHireDate) {}
-
-
-    // Accessor functions
-    // for name
-    std::string getName() const {
-        return name;
-    }
-
-    //mutator for name
-    void setName(const std::string& empName) {
-        name = empName;
-    }
-
-    //for idNumber
-    int getidNumber() const {
-        return idNumber;
-    }
-
-    //mutator for idNumber
-    void setidNumber(int empidNumber) {
-        idNumber = empidNumber;
-    }
-
-    //for hireDate
-    std::string gethireDate() const {
-        return hireDate;
-    }
-
-    //mutator for hireDate
-    void sethireDate(const std::string& empHireDate) {
-        hireDate = empHireDate;
-    }
-
-    //Print 
-    void print() const {
-        std::cout << "Employee Report" << "\n";
-        std::cout << "Name: " << name << "\n";
-        std::cout << "ID Number: " << idNumber << "\n";
-        std::cout << "Date of Hire" << hireDate << "\n";
+    //virtual print function
+    virtual void print() const {
+      cout << "Name: " << name << endl;
+      cout << "Employee ID: " << employeeID << endl;
+      cout << "Department: " << department << endl;
     }
 };
 
-//main function
+    //shift supervisor class derived
+    class ShiftSupervisor : public Employee {
+    private:
+        double salary;
+        double bonus;
+
+    public:
+        //constructor
+        ShiftSupervisor(string n = "", int id = 0, string dep = "", double salary = 0.0, double bonus = 0.0)
+        : Employee(n, id, dep), salary(salary), bonus(bonus) 
+{}
+
+        //accessors
+        double getSalary() const { return salary; }
+        double getBonus() const { return bonus; }
+
+        //mutators
+        void setSalary(double salary) { this->salary = salary; }
+        void setBonus(double bonus) { this->bonus = bonus; }
+
+        //print
+        virtual void print() const override {
+            Employee::print();
+            cout << "Annual Salary: $" << salary << endl;
+            cout << "Bonus: $" << bonus << endl;
+        }
+};
+
 int main() {
-    Employee emp1("Zachary Whippo", 12345, "2023-10-31");
-    emp1.print();
+    //create employee object
+    ShiftSupervisor supervisor("Chris Greene", 1234, "G.E Build", 75000.0, 5000.0);
+
+    cout << "Shift Supervisor Information:" << endl;
+    supervisor.print();
+
     return 0;
 }
